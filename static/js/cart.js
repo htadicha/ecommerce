caf1478721899e63149df81a2b32446e65155b9f
Hussein Tadicha <husseintadicha@Cexs-MacBook-Pro.local>
2024-10-08 11:25:01 +0100

    user logic added depending on whether the user is logged or not

--- static/js/cart.js
+++ static/js/cart.js
@@ -5,6 +5,15 @@ for (i = 0; i < updateBtns.length; i++) {
 		var productId = this.dataset.product
 		var action = this.dataset.action
 		console.log('productId:', productId, 'Action:', action)
+       
+        console.log('USER:', user)
+		if (user == 'AnonymousUser'){
+			console.log('User is not authenticated')
+			
+		}else{
+			console.log('User is authenticated, sending data...')
+		}
+
 
 	})
 }
