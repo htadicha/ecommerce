3c19092cabfa233e8b6f08a8ade197b3c005ee91
Hussein Tadicha <husseintadicha@Cexs-MacBook-Pro.local>
2024-10-08 10:38:58 +0100

    created views for authenticated and unauthenticated user

--- store/views.py
+++ store/views.py
@@ -3,12 +3,21 @@ from .models import *
 
 # Create your views here.
 def store(request):
-    products = Product.objects.all()
-    context = {'products':products}
-    return render(request, 'store/store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
 
 def cart(request):
-	context = {}
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+
+	context = {'items':items}
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
