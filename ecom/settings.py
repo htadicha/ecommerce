fe7f177c6d952b2b142cafa3ecc920daf36d5e67
Hussein Tadicha <husseintadicha@Cexs-MacBook-Pro.local>
2024-10-12 00:29:20 +0100

    added allowed hosts

--- ecom/settings.py
+++ ecom/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = 'django-insecure-57o8^$vjlm01+ul6no6p*n%$%7040+vpgwj@bc9w0cs!x=n5u_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 '.herokuapp.com']
 
 
 # Application definition
